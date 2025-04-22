e9f542ae7359306141918ccdbd1e65a32b42cab5
Brent Vervaet <brent.vervaet@student.hogent.be>
2025-04-22 18:28:15 +0200

    Implement solutions for Advent 15, 16, 17, 18, 19, 20, 21, 22, 23, and 24; add logic for parts 1 and 2, including grid manipulation and pathfinding algorithms; remove unused Advent 25 part 2 file.

---
+++ Advent 25/advent.25.1.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+
+def part1(puzzle_input):
+    locks_and_keys = defaultdict(list)
+    for seg in puzzle_input.split('\n\n'):
+        locks_and_keys[seg[0]].append([
+            col.count('#') for col in zip(*seg.split('\n'))
+        ])
+
+    fit = 0
+    for lock in locks_and_keys['.']:
+        for key in locks_and_keys['#']:
+            fit += all(i + j < 8 for i, j in zip(lock, key))
+
+    return fit
